fc05843fe087ff5358e77dbf48130292fb0d0c02
Luis Alves <luisalves05@gmail.com>
2017-04-09 13:49:47 -0300

    Correcting some styles

--- DinningPhilosophers/DinningPhilosophers.cpp
+++ DinningPhilosophers/DinningPhilosophers.cpp
@@ -5,30 +5,30 @@
 #include <chrono>
 #include <algorithm>
 
-#define N 5
+#define N   5
 
 std::mutex forks[N];
-std::mutex print_mu;
+std::mutex print_mu; // guards the cout << I/O
 
 void think(int i) {  
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::lock_guard<std::mutex> print_guard(print_mu);
+    std::this_thread::sleep_for (std::chrono::milliseconds {3000});
+    std::lock_guard<std::mutex> print_guard {print_mu};
     std::cout << "Philosopher #" << i << " - sleeping 3 seconds" << std::endl; 
 }
 
 void eat(int i) {  
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    std::lock_guard<std::mutex> print_guard(print_mu);
+    std::this_thread::sleep_for (std::chrono::milliseconds{2000});
+    std::lock_guard<std::mutex> print_guard{print_mu};
     std::cout << "Philosopher #" << i << " - eating 5 seconds" << std::endl; 
 }
 
-void init(int i) {
+void philosopher(int i) {
     while (true) { 
-        think(i);
+        think(i); // therefore, I am
         std::unique_lock<std::mutex> right_fork (
-                                        forks[std::min(i, (i + 1) % N)]);
+                                        forks[ std::min(i, (i + 1) % N) ]);
         std::unique_lock<std::mutex> left_fork (
-                                        forks[std::max(i, (i + 1) % N)]);
+                                        forks[ std::max(i, (i + 1) % N) ]);
         eat(i); // pizza
         left_fork.unlock();
         right_fork.unlock();
@@ -38,7 +38,7 @@ void init(int i) {
 int main() {
     std::thread phils[N];
     for (size_t i = 0; i < N; i++) {
-        phils[i] = std::thread(init, i);
+        phils[i] = std::thread (philosopher, i);
     }
     for (size_t i = 0; i < N; i++) {
         phils[i].join();
